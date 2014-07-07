1c64170cf4a3ce3b68d823971f4bcfe6a9254a15
unknown <saumgarg@.corp.ebay.com>
2014-07-07 17:04:53 +0530

    LAZY genereating a grid

---
+++ Boggle/src/boggle/GenerateGrid.java
@@ -0,0 +1,21 @@
+package Boggle;
+
+import java.util.Random;
+
+public class GenerateGrid {
+    String[] alphabets;
+    String[][] grid;
+    GenerateGrid(){
+    	alphabets = new String[]{"A","B","c","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    	grid = new String[4][4];
+    }
+    String[][] fillGrid(){
+    	for (int i = 0; i < 4;i++){
+    		for(int j = 0;j < 4;j++){
+    			Random randomizer = new Random();
+    			grid[i][j] = alphabets[randomizer.nextInt(26)];
+    		}
+    	}
+    	return grid;
+    }
+  }
