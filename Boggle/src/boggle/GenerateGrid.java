ce21bba4755ca50713436560f013be378e08fd72
unknown <piverma@.corp.ebay.com>
2014-07-07 18:31:20 +0530

    Lazy code

--- Boggle/src/boggle/GenerateGrid.java
+++ Boggle/src/boggle/GenerateGrid.java
@@ -7,15 +7,15 @@ public class GenerateGrid {
     String[][] grid;
     public GenerateGrid(){
     	alphabets = new String[]{"A","B","c","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    	grid = new String[][]{{"R","I","R","S"},{"S","C","R","O"},{"O","A","R","O"},{"E","E","P","E"}};
+    	grid = new String[4][4];
     }
     public String[][] fillGrid(){
-    	/*for (int i = 0; i < 4;i++){
+    	for (int i = 0; i < 4;i++){
     		for(int j = 0;j < 4;j++){
     			Random randomizer = new Random();
     			grid[i][j] = alphabets[randomizer.nextInt(26)];
     		}
-    	}*/
+    	}
         
     	return grid;
     }
